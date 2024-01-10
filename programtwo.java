5d2d9b3f082fc80319272e5dbec22c807a3a09c1
Aditya <123793076+b-vaibhaw@users.noreply.github.com>
2024-01-10 22:54:06 +0530

    Add files via upload

---
+++ programtwo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class programtwo {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Enter the Number of Subjects:");
+        int subno = sn.nextInt();
+        float marks = 0;
+        float tm = 0;
+        for (float j = 1; j <= subno; j++) {
+            System.out.printf("Enter the Subject %.0f marks (in 100):\n", j);
+            marks = sn.nextFloat();
+            tm += marks;
+        }
+        sn.close();
+        System.out.printf("Total marks obtained is %.3f in %d .\n", tm, (subno * 100));
+        System.out.printf("Your average percentage in %d subject(s) is %.3f .\n", subno, (tm / subno));
+
+        if (marks / subno >= 90) {
+            System.out.println("Grade is A+.");
+        } else if (tm / subno >= 80) {
+            System.out.println("Grade is A.");
+        } else if (tm / subno >= 70) {
+            System.out.println("Grade is B+.");
+        } else if (tm / subno >= 60) {
+            System.out.println("Grade is B.");
+        } else {
+            System.out.println("You are Below 60.");
+        }
+    }
+}
